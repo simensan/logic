fdd4c2cfea92e5019998a03249369dea541bda8f
bronzon <olle.back@gmail.com>
2015-11-10 10:39:49 +0100

    Default project with rotating thingiemojig

---
+++ Logical/Assets/RotateOnClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateOnClick : MonoBehaviour {
+
+    void OnMouseDown() {
+        transform.Rotate(Vector3.forward, 45f);
+    }
+}
---
+++ Logical/Assets/scripts/ResizeCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class ResizeCamera : MonoBehaviour {
+
+    public float defaultAspect = 4 / 3f;
+
+    void Start() {
+        GetComponent<Camera>().orthographicSize = defaultAspect / GetComponent<Camera>().aspect * GetComponent<Camera>().orthographicSize;
+    }
+}
