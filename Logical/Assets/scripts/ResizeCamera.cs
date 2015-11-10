a055e330977ab34ea9a06e7ae95125711ac751fe
bronzon <olle.back@gmail.com>
2015-11-10 21:13:01 +0100

    music and aspect fix

--- Logical/Assets/scripts/ResizeCamera.cs
+++ Logical/Assets/scripts/ResizeCamera.cs
@@ -4,7 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(Camera))]
 public class ResizeCamera : MonoBehaviour {
 
-    public float defaultAspect = 4 / 3f;
+    public float defaultAspect = 16 / 9f;
 
     void Start() {
         GetComponent<Camera>().orthographicSize = defaultAspect / GetComponent<Camera>().aspect * GetComponent<Camera>().orthographicSize;
